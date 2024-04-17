dbe2e91a22a7afc0ea39354465f0a27380dbe9cd
jtschams <jtschams@yahoo.com>
2024-04-17 13:22:13 -0500

    sorted parkData by distance. added fetch for weather of closest parks. added and populated weather object as parkData property

--- assets/js/api-calls.js
+++ assets/js/api-calls.js
@@ -3,8 +3,9 @@
 // const parkApiParks = "https://developer.nps.gov/api/v1/parks";
 // const parkApiCamp = "https://developer.nps.gov/api/v1/campgrounds";
 // const parkKey = "nQU50CkQwFxE1hsRuWF2bTdGUZVlHWhfKj0MTDsN";
-const weatherApi = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?locations=${parkLat},${parkLon}&alertLevel=true&aggregateHours=24&unitGroup=us&contentType=json&key=SJJYSNFMDHASY352EUXQKNUP4`
+// const weatherApi = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timelinemulti?locations=${locationParameter}&include=days,alerts&datestart=${startDate}&dateend=${endDate}&unitGroup=us&contentType=json&key=SJJYSNFMDHASY352EUXQKNUP4`
 
+const parkData = [];  // was between 42 and 43
 
 // Fetch coordinates from Zip Code
 const getLocation = function(input) {
@@ -20,7 +21,7 @@ const getLocation = function(input) {
             lon: data.results[0].geometry.location.lng
         }
         // ! REMOVE CONSOLE LOG
-        console.log(homeCoords);
+        console.log('get location', homeCoords);
         parkLocation(homeCoords);
     })
 }
@@ -29,7 +30,7 @@ const getLocation = function(input) {
 const parkLocation = function(home) {
     fetch('https://developer.nps.gov/api/v1/parks?limit=1000', {
         headers: {
-            "X;Api-Key": "nQU50CkQwFxE1hsRuWF2bTdGUZVlHWhfKj0MTDsN"
+            "X-Api-Key": "nQU50CkQwFxE1hsRuWF2bTdGUZVlHWhfKj0MTDsN"
         }
     })
     .then(function(response) {
@@ -37,9 +38,8 @@ const parkLocation = function(home) {
     })
     .then(function(data) {
         // ! REMOVE CONSOLE LOG
-        console.log(data);
+        console.log('park location data', data);
         // Create Park Data Array
-        const parkData = [];
         for (park of data.data) {
             // ? Possibly filter by designation
             const parkInfo = {
@@ -47,13 +47,15 @@ const parkLocation = function(home) {
                 lat: park.latitude,
                 lon: park.longitude,
                 description: park.description,
-                address: park.addresses[0]
+                address: park.addresses[0],
+                parkCode: park.parkCode
             }
             parkData.push(parkInfo);
         }
         // ! REMOVE CONSOLE LOG
-        console.log(parkData);
+        console.log('filled parkData array', parkData);
         parkSorting(home, parkData);
+        getWeather(parkData);
     })
 }
 
@@ -65,10 +67,46 @@ const parkSorting = function(homeCoords, parkData) {
         parkData[i].distance = Math.sqrt(latDist * latDist + lonDist * lonDist);
     }
     // ! REMOVE CONSOLE LOG
-    console.log(parkData);
-    // TODO: Sort by Distance
+    console.log('parkData with distance', parkData);
+    parkData.sort((a, b) => a.distance - b.distance)
+    console.log('sorted parkData', parkData);
+    parkData.splice(5, parks.length - 5)
+    console.log('reduced parkData', parkData);
 }
 
 // get rainfall on closest 4-10 parks
+const getWeather = function(parkData) {
+    let locationParameter = '';
+    for (park of parkData) {
+        locationParameter = locationParameter.concat(`|${park.lat},${park.lon}`);
+        park.parkWeather = {};
+    }
+    locationParameter = locationParameter.slice(1);
+    let weatherURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timelinemulti?locations=${locationParameter}&include=days,alerts&datestart=${startDate}&dateend=${endDate}&unitGroup=us&contentType=json&key=SJJYSNFMDHASY352EUXQKNUP4`;
+    fetch(weatherURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        // Creates and overwrites data
+        // ! DELETE CONSOLE LOG
+        console.log('weather data', data)
+
+        for (i = 0; i < parkData.length; i++) {
+            for (j = 0; j < data.locations[i].days.length; j++) {
+                if (!parkData[i].parkWeather.chance || parkData[i].parkWeather.chance < data.locations[i].days[j].precipprob) {
+                    parkData[i].parkWeather.chance = data.locations[i].days[j].precipprob;
+                    parkData[i].parkWeather.cover = data.locations[i].days[j].precipcover;
+                    parkData[i].parkWeather.amount = data.locations[i].days[j].precip;
+                    parkData[i].parkWeather.date = data.locations[i].days[j].datetime;
+                }
+            }
+        }
+    })
+    .then(function() {
+        // ! DELETE CONSOLE LOG
+        console.log(parkData)
+    })
+}
 
 
