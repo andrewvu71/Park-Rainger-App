74a71b3687aad1fad83427517373045fb975be77
jtschams <jtschams@yahoo.com>
2024-04-18 20:43:08 -0500

    fixed park card display.  added github links.  removed unused files. cleaned up code.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,11 @@
 //Declaring variables
 
-//const cardLink= document.getElementById('modalLink');
 const modal= document.getElementById('modal1');
 const btnSearch= document.getElementById('btnSearch');
 const weatherCards= document.getElementById('weatherCard');
 const zipcode= document.getElementById('txtZipcode');
 const parentDivId= document.getElementById('parentDiv');
-
+const resultsTitle= document.getElementById('results-title');
 
 const parkData = [];
 
@@ -14,15 +13,16 @@ const parkData = [];
 function getParkDetails(event)
 { 
     event.preventDefault();
-
-    // TODO: Change this line to select based on which card was clicked
-    let parkIndex = 0;
+    
     //this condition is to reset elements in modal. It deletes all previous elements in modal 
     if(modal.hasChildNodes)
     {
         modal.replaceChildren("");
     }
-
+    
+    // Selects the specific 
+    const cardID = event.target.getAttribute('id')
+    const parkIndex = cardID.charAt(cardID.length - 1);
     //create modal elements
     const divModal = document.createElement("div");
     
@@ -31,7 +31,7 @@ function getParkDetails(event)
     const h2 = document.createElement("h2");
     h2.textContent="Park Details";
     divModal.append(h2);
-    const modalParkName = document.createElement("h6")
+    const modalParkName = document.createElement("h4")
     modalParkName.textContent = parkData[parkIndex].name;
     divModal.append(modalParkName);
     const address = document.createElement("h5");
@@ -57,7 +57,8 @@ function getParkDetails(event)
     divModal.append(direction);
     const directionURL= document.createElement("a");
     directionURL.textContent="Click here for directions.";
-    directionURL.setAttribute('href', parkData[parkIndex].directions);
+    directionURL.setAttribute("href", parkData[parkIndex].directions);
+    directionURL.setAttribute("target", "_blank");
     divModal.append(directionURL);
     modal.append(divModal);
     //footer
@@ -84,115 +85,39 @@ function getWeatherDetails(parkData)
 {
     for (i = 0; i < parkData.length; i++) {
         const cardDiv= document.createElement("div");
-        cardDiv.innerHTML=` <div class="row">
-                        <div class="col s12 m6">
-                            <div class="card">
-                                <div class="card-image">
-                                    <img src=${parkData[i].image}>
-                                   
-                                </div>
-                                <div class="card-content">
-                                    <h5>Park name: ${parkData[i].name}</h5>
-                                    <h5>Rain Chance: ${parkData[i].parkWeather.chance}</h5>
-                                    <h5>Rain Amount: ${parkData[i].parkWeather.amount}</h5>
-                                    <h5>Rain Date:${parkData[i].parkWeather.date}</h5>
-                                </div>
-                            <div class="card-action">
-                            
-                            <button id="park-card-${i}" class="btnParkDetail btn waves-effect">Click here for more details!</button>
-                          </div>
+        cardDiv.innerHTML=`
+        <div>
+            <div class="col s12 m6">
+                <div class="card">
+                    <div class="card-image">
+                        <img src=${parkData[i].image}>
                     </div>
-                </div>`;
-            
+                    <div class="card-content">
+                        <h5 class="center-align">${parkData[i].name}</h5>
+                        <h5>Rain Chance: ${parkData[i].parkWeather.chance}%</h5>
+                        <h5>Rain Amount: ${parkData[i].parkWeather.amount} inches</h5>
+                        <h5>Rain Date:${parkData[i].parkWeather.date}</h5>
+                    </div>
+                    <div class="card-action center-align">
+                        <button id="park-card-${i}" class="btnParkDetail btn waves-effect">Click here for more details!</button>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+        
         parentDivId.append(cardDiv);  
         cardButton = document.getElementById(`park-card-${i}`);
         cardButton.addEventListener('click', getParkDetails);
-    }
-    
-    
-    // const cardDiv= document.createElement("div");
-    // cardDiv.innerHTML=` <div class="row">
-    //                 <div class="col s12 m3">
-    //                     <div class="card">
-    //                         <div class="card-image">
-    //                             <img src=${image}>
-                               
-    //                         </div>
-    //                         <div class="card-content">
-    //                             <h3>Park name: ${name}</h3>
-    //                             <h3>Rain Chance: ${chance}</h3>
-    //                             <h3>Rain Amount: ${amount}</h3>
-    //                             <h3>Rain Date:${date}</h3>
-    //                         </div>
-    //                     <div class="card-action">
-                        
-    //                     <button id="btnParkDetail" class="btn waves-effect">Click here for more details!</button>
-    //                   </div>
-    //             </div>
-    //         </div>`;
-        
-    // parentDivId.append(cardDiv);      
-            
-      
-
-            //   const divParentcard = document.createElement("div");
-            //   divParentcard.classList.add("col");
-            //   divParentcard.classList.add("s12");
-            //   divParentcard.classList.add("m3");
-            //   parentDivId.append(divParentcard);
-
-//   const divcard = document.createElement("div");
-//   divcard.classList.add("card");
-//   divParentcard.append(divcard);
-//   const cardImagediv = document.createElement("div");
-//   cardImagediv.classList.add("card-image");
-//   divcard.append(cardImagediv);
-//   const cardImage = document.createElement("img");
-//   cardImage.src=image;
-//   cardImagediv.append(cardImage);
-//   const cardTitle = document.createElement("span");
-//   cardTitle.classList.add('card-title');
-//   cardTitle.textContent="Card Title" //add park name here from API
-//   cardImagediv.append(cardTitle);
-  
-//   const cardContent= document.createElement("div");
-//   cardContent.classList.add('card-content');
-//   divcard.append(cardContent);
-//   const Parkname= document.createElement("h3");
-//   Parkname.textContent="Park Name:"+ name;
-//   cardContent.append('Parkname');
-//   const rainChance= document.createElement("h3");
-//   rainChance.textContent="RainChance:"+rainChance;
-//   cardContent.append('rainChance');
-//   const rainamount= document.createElement("h3");
-//   rainamount.textContent="RainAmount:"+rainamount;
-//   cardContent.append('rainamount');
-//   const rainDate= document.createElement("h3");
-//   rainDate.textContent="RainDate:"+rainDate;
-//   cardContent.append('rainDate');
-  
-//   const cardAction= document.createElement("div");
-//   cardAction.classList.add('card-action');
-//   divcard.append(cardAction);
-  
-//   const cardLink= document.createElement("a");
-//   cardLink.id='modalLink';
-//   cardLink.classList.add('waves-effect');
-//   cardLink.classList.add('waves-teal');
-//   cardLink.classList.add('btn-flat');
-//   cardLink.textContent='Click here to know more';
-//   cardLink.addEventListener('click',getParkDetails);
-//   cardAction.append(cardLink);
+    }    
 }
 
 
 // Fetch coordinates from Zip Code
 const getLocation = function(event) {
-
     event.preventDefault();
+    
+    resultsTitle.textContent = "Finding parks near you with rain..."
     const homeLocation = zipcode.value.trim();
-   // const homeLocation = zipcode.textContent.trim();
-   console.log(homeLocation)
     fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${homeLocation}&key=AIzaSyA6_fb9VakKHBcqkyryNKgwbmO6CsVGGnQ`)
     .then(function(response) {
         return response.json();
@@ -247,9 +172,8 @@ const parkSorting = function(homeCoords, parkData) {
         parkData[i].distance = Math.sqrt(latDist * latDist + lonDist * lonDist);
     }
     parkData.sort((a, b) => a.distance - b.distance);
-    // * Sets the number of parks to check for rain
-    parkData.splice(4, parkData.length - 4);
-    i = 0;
+    // * Sets the number of nearby parks to check for rain.  Must be at least 4
+    parkData.splice(8, parkData.length - 8);
 }
 
 // get rainfall on closest 4-10 parks
@@ -266,27 +190,16 @@ const getWeather = function(parkData) {
             date: ''
         };
     }
-    let loopFetch = setInterval(function() {
-        if (!i) {
-            let i = 0;
-        }
+    for (i = 0; i < locationParameters.length; i++) {
         let weatherURL = `https://api.tomorrow.io/v4/weather/forecast?location=${locationParameters[i]}&timesteps=1d&units=imperial&apikey=${weatherAPIs[i]}`;
         weatherResponses.push(fetch(weatherURL));
-        i++;
-        if (i >= locationParameters.length) {
-            clearInterval(loopFetch);
-        }
-    }, 350)
-    let fetchTimer = 350 * locationParameters.length + 10;
-    
-    setTimeout(() => {Promise.all(weatherResponses)
+    }
+
+    Promise.all(weatherResponses)
         .then(function(responses) {
             const parsedResponses = [];
             for (i = 0; i < weatherResponses.length; i++) {
-
-                console.log(i);
                 let response = responses[i].json();
-
                 parsedResponses.push(response);
             }
             Promise.all(parsedResponses)
@@ -298,24 +211,18 @@ const getWeather = function(parkData) {
                             parkData[i].parkWeather.chance = data[i].timelines.daily[j].values.precipitationProbabilityMax;
                             parkData[i].parkWeather.amount = data[i].timelines.daily[j].values.rainAccumulationSum;
                             parkData[i].parkWeather.date = data[i].timelines.daily[j].time;
-                            console.log(parkData[i]);
-                            // getWeatherDetails(parkData[i].parkWeather.chance,parkData[i].parkWeather.amount, parkData[i].parkWeather.date,parkData[i].name,parkData[i].image);// calling this function to display details in weather card
                         }
                     }
                 }
                 parkData.sort((a, b) => b.parkWeather.chance - a.parkWeather.chance)
-                // ! REMOVE CONSOLE LOGS
-                console.log('sorted by weather', parkData)
                 if (parkData.length > 4) {
                     parkData.splice(4, parkData.length - 4)
                 }
-               getWeatherDetails(parkData);
+                resultsTitle.textContent = "National Parks with Rain in their Forecast"
+                getWeatherDetails(parkData);
             })
         })
-    }, fetchTimer)
 }
-
-// btnSearch.addEventListener('click',getWeatherDetails);
 
 btnSearch.addEventListener('click',getLocation);
 
